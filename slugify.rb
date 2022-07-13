f32f7d99c8b3b1cdbd687fb0de41ad9c7a283e51
Guillermo Siliceo <98428834+grillermo-everlyhealth@users.noreply.github.com>
2022-07-13 11:00:25 -0500

    Initial commit

---
+++ slugify.rb
@@ -0,0 +1,19 @@
+# #!/usr/bin/env ruby
+#
+# To use this script with system ruby
+# rbenv shell system
+# sudo gem install clipboard string_helpers
+#
+require 'clipboard'
+require 'string_helpers'
+
+def remove_diacritics(str)
+  str.tr(
+    "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+    "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
+  )
+end
+
+transformed = Clipboard.paste.slug!
+
+Clipboard.copy(remove_diacritics(transformed))
